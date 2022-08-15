a5e7b2b1adb67c6f402348c49056b9dc82c40084
Maykon de Castro Mendel <maykonmendel.dev@gmail.com>
2022-08-15 16:09:46 -0300

    Add RepositoriesPage and route

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdSearch } from 'react-icons/md';
 import { Container, LogoGithub, Title, Form, Input, Button } from './styles';
 import GitHubLogo from '../../assets/images/github-logo.svg';
 
-const MainPage = () => (
-  <Container>
-    <LogoGithub src={GitHubLogo} alt="API Github" />
-    <Title>API Github</Title>
-    <Form>
-      <Input placeholder="usuário" />
-      <Button>
-        <MdSearch size={42} />
-      </Button>
-    </Form>
-  </Container>
-);
+const MainPage = () => {
+  const [login, setLogin] = useState('');
+
+  return (
+    <Container>
+      <LogoGithub src={GitHubLogo} alt="API Github" />
+      <Title>API Github</Title>
+      <Form>
+        <Input
+          placeholder="usuário"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <Button to={`/${login}/repositories`}>
+          <MdSearch size={42} />
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default MainPage;
